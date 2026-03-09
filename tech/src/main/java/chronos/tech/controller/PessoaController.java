3efca0cf2c9de30da331f49b0a1b211d2d1a64a3
Daniel-Costa-Avelino <daniel.avelino@sptech.school>
2026-03-09 18:26:52 -0300

    feat: CRUD completo de Pessoa

--- tech/src/main/java/chronos/tech/controller/PessoaController.java
+++ tech/src/main/java/chronos/tech/controller/PessoaController.java
@@ -4,11 +4,11 @@ import chronos.tech.model.classes.Pessoa;
 import chronos.tech.service.PessoaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/pessoas")
@@ -22,6 +22,30 @@ public class PessoaController {
 
         return ResponseEntity.ok().body(service.getAllPersons());
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Optional<Pessoa>> getPessoa(@PathVariable @Validated Long id){
+        var resposta = service.getPessoa(id);
+        return ResponseEntity.ok().body(resposta);
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> savePessoa(@RequestBody @Validated Pessoa pessoa){
+        service.savePessoa(pessoa);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<String> updatePessoa(@PathVariable @Validated Long id, @RequestBody Pessoa pessoa){
+        service.updatePessoa(id, pessoa);
+        return ResponseEntity.ok().body("Deu certo");
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deletePessoa(@PathVariable @Validated Long id){
+        service.deletePessoa(id);
+        return ResponseEntity.ok().build();
     }
 
 }
--- tech/src/main/java/chronos/tech/service/PessoaService.java
+++ tech/src/main/java/chronos/tech/service/PessoaService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PessoaService {
@@ -13,11 +14,45 @@ public class PessoaService {
     @Autowired
     private PessoaRepository repository;
 
+    //Método para pegar todas as pessoas
     public List<Pessoa> getAllPersons() {
 
         var allPersons = repository.findAll();
         return allPersons;
 
+    }
+
+    //Método pegar uma pessoa específico
+    public Optional<Pessoa> getPessoa(Long id){
+
+        return repository.findById(id);
+
+    }
+
+    //Salvar no banco de dados h2
+    public Pessoa savePessoa(Pessoa pessoa){
+        return repository.save(pessoa);
+    }
+
+    //Atualizar a pessoa
+    public Pessoa updatePessoa(Long id, Pessoa pessoaAtualizado){
+        Pessoa pessoaExistente = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Turma não encontrado com o ID: " + id));
+
+        if (pessoaAtualizado.getNome() != null) pessoaExistente.setNome(pessoaAtualizado.getNome());
+        if (pessoaAtualizado.getGenero() != null) pessoaExistente.setGenero(pessoaAtualizado.getGenero());
+        if (pessoaAtualizado.getTipoVinculo() != null) pessoaExistente.setTipoVinculo(pessoaAtualizado.getTipoVinculo());
+        if (pessoaAtualizado.getData_nascimento() != null) pessoaExistente.setData_nascimento(pessoaAtualizado.getData_nascimento());
+        if (pessoaAtualizado.getData_ingresso() != null) pessoaExistente.setData_ingresso(pessoaAtualizado.getData_ingresso());
+        if (pessoaAtualizado.getData_membro() != null) pessoaExistente.setData_membro(pessoaAtualizado.getData_membro());
+        if (pessoaAtualizado.getData_saida() != null) pessoaExistente.setData_saida(pessoaAtualizado.getData_saida());
+
+        return repository.save(pessoaExistente);
+    }
+
+    //Deletar uma pessoa
+    public void deletePessoa(Long id){
+        repository.deleteById(id);
     }
 
 }
